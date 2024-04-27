3203452176e1f13fbde8c7ac1114c7d0c4458c11
s25300 <adamsiedlec@gmail.com>
2024-04-27 15:57:36 +0200

    specyfikacja testów dla "mini projektu 2"

---
+++ src/test/groovy/MiniProjekt2Spec.groovy
@@ -0,0 +1,24 @@
+import spock.lang.Specification
+
+/**
+ *
+ w każdym przypadku: liczności
+ 1-* lub *-* oraz automatyczne
+ tworzenie poł. zwrotnego)
+ */
+
+class MiniProjekt2Spec extends Specification {
+
+    def "powinien mieć asocjację 'zwykłą' "() {
+    }
+
+    def "powinien mieć asocjację z atrybutem "() {
+    }
+
+    def "powinien mieć asocjację kwalifikowaną "() {
+    }
+
+    def "powinien mieć kompozycję"() {
+    }
+
+}
