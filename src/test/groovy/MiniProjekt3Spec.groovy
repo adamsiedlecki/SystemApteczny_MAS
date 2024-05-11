cb56069ca335ed5d7cf5bb3b5b7362e2b5a4fcf8
Adam <adamsiedlec@gmail.com>
2024-05-11 16:39:57 +0200

    specyfikacja projektu 3

---
+++ src/test/groovy/MiniProjekt3Spec.groovy
@@ -0,0 +1,25 @@
+import spock.lang.Specification
+
+class MiniProjekt3Spec extends Specification {
+
+    def "powinna być klasa abstrakcyjna / polimorfizm"() {
+
+    }
+
+    def "powinien być overlapping"() {
+
+    }
+
+    def "powinno być wielodziedziczenie"() {
+
+    }
+
+    def "powinno być wielodziedziczenie wieloaspektowe"() {
+
+    }
+
+    def "powinno być wielodziedziczenie dynamiczne"() {
+
+    }
+
+}
